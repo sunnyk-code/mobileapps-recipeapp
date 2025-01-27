b1e128c3f0a20b270aa6616c424150027b81ae47
Ethan Li <esli@gatech.edu>
2025-01-27 11:38:49 -0500

    firebase config small fix

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+//hidden firebase sensitive data
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
